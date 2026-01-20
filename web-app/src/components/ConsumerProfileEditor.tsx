1da5c3e702dc9d2b7f63d2032455a40ec47fe127
Liam McKeown <liam.mckeown38415@gmail.com>
2026-01-19 18:51:13 -0800

    Comment out Promotional Emails toggle

--- web-app/src/components/ConsumerProfileEditor.tsx
+++ web-app/src/components/ConsumerProfileEditor.tsx
@@ -471,6 +471,7 @@ const ConsumerProfileEditor = forwardRef<ConsumerProfileEditorRef, ConsumerProfi
                 </label>
               </div>
 
+              {/* Promotional Emails - commented out for now
               <div className="flex items-center justify-between p-4 bg-gray-50 rounded-lg">
                 <div>
                   <p className="font-medium text-gray-900">Promotional Emails</p>
@@ -486,6 +487,7 @@ const ConsumerProfileEditor = forwardRef<ConsumerProfileEditorRef, ConsumerProfi
                   <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-primary-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-primary-400"></div>
                 </label>
               </div>
+              */}
             </div>
           </Card>
 
